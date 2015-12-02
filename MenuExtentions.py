4681b989dc75c7829ffd653f41749bf862fb19b3
sahar <sahar@dooble.co.il>
2015-12-02 16:14:21 +0200

    idan updates

--- MenuExtentions.py
+++ MenuExtentions.py
@@ -1,6 +1,7 @@
 import sublime, sublime_plugin
 import re
 import os
+import ntpath
 #TODO: concentrate filepath in same class
 #TODO: why check ftp permissions fall sometimes
 class DoobleIO():
@@ -56,7 +57,7 @@ class DoobleIO():
 
 		return filePath
 		
-	#---------------------------------------------------------------------------------------------------------------
+	
 	@staticmethod
 	def get_master_path(file_name, result):
 		left_path, file_name = DoobleIO.cut_path(file_name)
@@ -75,7 +76,7 @@ class DoobleIO():
 				break
 			root += i + '\\'
 		root += const_path + result
-		print(left_path + root)
+		# print(left_path + root)
 		return left_path + root
 
 	@staticmethod
@@ -100,7 +101,7 @@ class DoobleIO():
 				break
 			root += lis[i] + '\\'
 		root += const_path + result
-		print(left_path + root)
+		# print(left_path + root)
 		return left_path + root
 
 
@@ -150,8 +151,15 @@ class DoobleIO():
 
 		return(left_path, right_path)
 
+	@staticmethod
+	def is_folder(path):
+		if os.path.isdir(path):
+			return True
+		return False
+
+
 class AddItemCommand(sublime_plugin.WindowCommand):
-	def run(self, files):
+	def run(self, files=[], paths=[]):
 		if not files:
 			files.append(sublime.active_window().active_view().file_name())
 		# get file path
@@ -162,7 +170,11 @@ class AddItemCommand(sublime_plugin.WindowCommand):
 		#goes to new file
 		sublime.active_window().open_file(filePath)
 
-	def is_visible(self,files):
+	def is_visible(self, files=[], paths=[]):
+		# check if it's folder
+		if len(paths) == 1:
+			if DoobleIO.is_folder(paths[0]):
+				return False
 		if not files:
 			files.append(sublime.active_window().active_view().file_name())
 		if(len(files) > 1):
@@ -228,10 +240,27 @@ class GoToMasterCommand(sublime_plugin.WindowCommand):
 
 class AddConfigCommand(sublime_plugin.WindowCommand):
 
-	def run(self, files):
+	def run(self, files=[], paths=[]):
+		# In case it's a folder
+		if len(paths) == 1:
+			a_dir = paths[0]
+			if DoobleIO.is_folder(a_dir):
+				file_name = a_dir + '\\$.config'
+				# if os.path.isfile(file_name):
+				# 	sublime.active_window().open_file(file_name)
+				# else:
+				# 	# if the file does not exist, create one.
+				# 	open(file_name, 'a')
+				# 	sublime.active_window().open_file(file_name)
+				if not os.path.isfile(file_name):
+					open(file_name, 'a')
+					sublime.active_window().open_file(file_name)
+				
+			
+
 		if not files:
 			files.append(sublime.active_window().active_view().file_name())
-		#gets the file path
+		#get the file path
 		filePath = DoobleIO.getConfigPath(files)
 
 		folder = os.path.dirname(filePath)
@@ -244,14 +273,20 @@ class AddConfigCommand(sublime_plugin.WindowCommand):
    				sublime.error_message('There\'s a problem with the file manager. the file may already exist or Expendrive should be restarted.')
 
 
-	def is_visible(self,files):
+	def is_visible(self, files=[], paths=[]):
+		# In case it's a folder
+		if len(paths) == 1:
+			if DoobleIO.is_folder(paths[0]):
+				if os.path.isfile(paths[0] + '\\$.config'):
+					return False
+		# In case the files list is empty
 		if not files:
 			files.append(sublime.active_window().active_view().file_name())
-
-		# file = 
+		# In case the user make multiply selections
 		if(len(files) > 1):
 			return False
-		if("config" in files[0].lower()) :
+		
+		if("config" in files[0].lower()):
 			return False
 
 		filePath = DoobleIO.getConfigPath(files)
@@ -259,8 +294,15 @@ class AddConfigCommand(sublime_plugin.WindowCommand):
 			return False
 		return True
 
+
 class GoToConfigCommand(sublime_plugin.WindowCommand):
-	def run(self, files):
+	def run(self, files=[], paths=[]):
+		if len(paths) == 1:
+			if DoobleIO.is_folder(paths[0]):
+				file_name = paths[0] + '\\$.config'
+				if (os.path.isfile(file_name)):
+					sublime.active_window().open_file(file_name)
+
 		if not files:
 			files.append(sublime.active_window().active_view().file_name())
 
@@ -268,7 +310,13 @@ class GoToConfigCommand(sublime_plugin.WindowCommand):
 		sublime.active_window().open_file(filePath)
 
 
-	def is_visible(self,files):
+	def is_visible(self, files=[], paths=[]):
+		# In case it's a folder
+		if len(paths) == 1:
+			if DoobleIO.is_folder(paths[0]):
+				if (os.path.isfile(paths[0] + '\\$.config')):
+					return True
+
 		if not files:
 			files.append(sublime.active_window().active_view().file_name())
 		if(len(files) > 1):
@@ -283,35 +331,40 @@ class GoToConfigCommand(sublime_plugin.WindowCommand):
 
 		return True
 		
-#-----------------------------------------------------------------------------------------------------------------------	
+
 class GoToModuleCommand(sublime_plugin.WindowCommand):
 
 	# [[Module/template]] or [[Module/template attr=""]]
-	REGEX = r"\[\[(\w+/\w+).*((.|\n)*)\]\]"
+	REGEX = r".*\[\[(\w+/\w+).*((.|\n)*)\]\].*"
 
 	def run(self):
 		# start the program
 		result = self.start()
-		global list_of_files
-		list_of_files = []
-		# note: if string or array empty the result return false
-		# if the result is string its single file
-		# else, its an array
+
 		if result:
-			if isinstance(result, str):
-				# open the file in new window
-				self.window.open_file(result) 
-			else:
-				list_of_files = result
-				panel_list = self.quick_panel_list(list_of_files)
-				# open the quick panel
-				self.window.show_quick_panel(panel_list, self.on_done, sublime.MONOSPACE_FONT)
-		else:
-			sublime.message_dialog("File not found")
+			sublime.active_window().run_command("show_overlay", {"overlay": "goto", "show_files": True, "text":result})
+
+
+		# global list_of_files
+		# list_of_files = []
+		# # note: if string or array empty the result return false
+		# # if the result is string its single file
+		# # else, its an array
+		# if result:
+		# 	if isinstance(result, str):
+		# 		# open the file in new window
+		# 		self.window.open_file(result) 
+		# 	else:
+		# 		list_of_files = result
+		# 		panel_list = self.quick_panel_list(list_of_files)
+		# 		# open the quick panel
+		# 		self.window.show_quick_panel(panel_list, self.on_done, sublime.MONOSPACE_FONT)
+		# else:
+		# 	sublime.message_dialog("File not found")
 
 			
-	def on_done(self, result):
-		self.window.open_file(list_of_files[result])	
+	# def on_done(self, result):
+	# 	self.window.open_file(list_of_files[result])	
 
 
 	# def is_visible(self):
@@ -323,17 +376,17 @@ class GoToModuleCommand(sublime_plugin.WindowCommand):
 	# 	return False
 		
 
-	def is_file(self, path):
-		fixed_path = ""
+	# def is_file(self, path):
+	# 	fixed_path = ""
 
-		if os.path.isfile(path + ".html"):
-			fixed_path += path + ".html"
-		elif os.path.isfile(path + ".htm"):
-			fixed_path += path + ".htm"
-		elif os.path.isfile(path + ".hpp"):
-			fixed_path += path + ".hpp"
+	# 	if os.path.isfile(path + ".html"):
+	# 		fixed_path += path + ".html"
+	# 	elif os.path.isfile(path + ".htm"):
+	# 		fixed_path += path + ".htm"
+	# 	elif os.path.isfile(path + ".hpp"):
+	# 		fixed_path += path + ".hpp"
 		
-		return fixed_path
+	# 	return fixed_path
 
 	
 	def start(self):
@@ -343,26 +396,43 @@ class GoToModuleCommand(sublime_plugin.WindowCommand):
 		search_result = self.match_selection(sel)
 		# get current path of file
 		file_name = sublime.active_window().active_view().file_name().lower()
-		# check if the current file is master
-		if DoobleIO.is_master(file_name):
-			files_list = self.get_files_list(search_result)
-			if len(files_list) > 0:
-				return files_list
+		
+		print("result: " + search_result)
+		# print(len(search_result))
+		if search_result:
+			if DoobleIO.is_ui_controls(search_result):
+				search_result = 'uicontrols\\' + search_result.split('/')[1]
+			elif 'admin' in file_name:
+				search_result = 'admin\\' + search_result
+			elif 'content' in file_name:
+				search_result = 'modules\\' + search_result
 
-		# first try search in site folder
-		result = self.is_site_path(file_name, search_result)
-		# if the file site exist
-		if result:
-			return result
+			search_result = search_result.replace('/', '\\')
+		
+		return search_result 
 
-		# if its not in site folder try search in master folder
-		result = self.is_master_path(file_name, search_result)
 
-		if result:
-			return result
 
-		# in case the file not exist at all
-		return ""
+		# # check if the current file is master
+		# if DoobleIO.is_master(file_name):
+		# 	files_list = self.get_files_list(search_result)
+		# 	if len(files_list) > 0:
+		# 		return files_list
+
+		# # first try search in site folder
+		# result = self.is_site_path(file_name, search_result)
+		# # if the file site exist
+		# if result:
+		# 	return result
+
+		# # if its not in site folder try search in master folder
+		# result = self.is_master_path(file_name, search_result)
+
+		# if result:
+		# 	return result
+
+		# # in case the file not exist at all
+		# return ""
 
 	def user_selection(self):
 		# get current view
@@ -371,97 +441,101 @@ class GoToModuleCommand(sublime_plugin.WindowCommand):
 		sel = view.sel()[0].begin()
 		# get the line with current cursor
 		get_line = view.line(sel)
+		print("the line: " + view.substr(get_line).strip())
 		# get the start line region
-		start_position = get_line.a
+		# start_position = get_line.a
 		# find the regex from the current cursor until you get the whole exp
-		sel = view.find(self.REGEX, start_position)
+		# sel = view.find(self.REGEX, start_position)
 		# get the selection in string
-		sel = view.substr(sel)
+		# sel = view.substr(sel)
+		# return sel
 
-		return sel
+		return view.substr(get_line).strip()
 
 	def match_selection(self, sel):
+		print("sel: " + sel)
 		try:
 			# do match with the selected exp
-			search_result = re.match(self.REGEX,sel).group(1)
+			search_result = re.match(self.REGEX, sel).group(1)
 		except:
 			# in case the sel = None
+			print("there is no match")
 			search_result = ""
 
 		return search_result.lower()
 
-	def is_site_path(self, file_name, result):
-		# get the site fixed path
-		site_fixed_path = DoobleIO.get_site_path(file_name, result)
-		fixed_path = self.is_file(site_fixed_path)
+	# def is_site_path(self, file_name, result):
+	# 	# get the site fixed path
+	# 	site_fixed_path = DoobleIO.get_site_path(file_name, result)
+	# 	fixed_path = self.is_file(site_fixed_path)
 
-		return fixed_path
+	# 	return fixed_path
 
-	def is_master_path(self, file_name, result):
-		# get the master fixed path
-		master_fixed_path = DoobleIO.get_master_path(file_name, result)
-		fixed_path = self.is_file(master_fixed_path)
+	# def is_master_path(self, file_name, result):
+	# 	# get the master fixed path
+	# 	master_fixed_path = DoobleIO.get_master_path(file_name, result)
+	# 	fixed_path = self.is_file(master_fixed_path)
 
-		return fixed_path
+	# 	return fixed_path
 
-	def quick_panel_list(self, list_of_files):
-		view_list = []
-		for item in list_of_files:
-			# this split create two parts: one with the root path
-			# and the other with name of site and so on
-		  if 'sites' in item.lower():
-		  	file_format = item.lower().split('sites\\')
-		  	# get the name of site and module name
-		  	view_list.append(file_format[1])
-		  elif 'site' in item.lower():
-		  	file_format = item.lower().split('site\\')
-		  	# get the name of site and module name
-		  	view_list.append('site\\' + file_format[1])
-		  else:
-		  	view_list.append("This is NOT site")
+	# def quick_panel_list(self, list_of_files):
+	# 	view_list = []
+	# 	for item in list_of_files:
+	# 		# this split create two parts: one with the root path
+	# 		# and the other with name of site and so on
+	# 	  if 'sites' in item.lower():
+	# 	  	file_format = item.lower().split('sites\\')
+	# 	  	# get the name of site and module name
+	# 	  	view_list.append(file_format[1])
+	# 	  elif 'site' in item.lower():
+	# 	  	file_format = item.lower().split('site\\')
+	# 	  	# get the name of site and module name
+	# 	  	view_list.append('site\\' + file_format[1])
+	# 	  else:
+	# 	  	view_list.append("This is NOT site")
 
-		return view_list
+	# 	return view_list
 
-	def get_files_list(self, search_result):
-		# in case the search_result pattern is [[ui/template]]
-		if DoobleIO.is_ui_controls(search_result):
-			search_result = DoobleIO.ui_format(search_result)
-		search_result = search_result.replace('/', '\\')
-		# get the open folders from the side bar
-		open_folders = self.get_open_folders() 
+	# def get_files_list(self, search_result):
+	# 	# in case the search_result pattern is [[ui/template]]
+	# 	if DoobleIO.is_ui_controls(search_result):
+	# 		search_result = DoobleIO.ui_format(search_result)
+	# 	search_result = search_result.replace('/', '\\')
+	# 	# get the open folders from the side bar
+	# 	open_folders = self.get_open_folders() 
 		 
-		files_list = []
-		for folder in open_folders:
-			# in case the search result is ui controls
-			result = self.is_file(folder + '\\' + search_result)
-			if result:
-				files_list.append(result)
-			# in case the file in admin folder
-			result = self.is_file(folder + '\\admin\\' + search_result)
-			if result:
-				files_list.append(result)
-			# in case the file in content folder
-			result = self.is_file(folder + '\\' + 'content\\modules\\' + search_result)
-			if result:
-				files_list.append(result)
+	# 	files_list = []
+	# 	for folder in open_folders:
+	# 		# in case the search result is ui controls
+	# 		result = self.is_file(folder + '\\' + search_result)
+	# 		if result:
+	# 			files_list.append(result)
+	# 		# in case the file in admin folder
+	# 		result = self.is_file(folder + '\\admin\\' + search_result)
+	# 		if result:
+	# 			files_list.append(result)
+	# 		# in case the file in content folder
+	# 		result = self.is_file(folder + '\\' + 'content\\modules\\' + search_result)
+	# 		if result:
+	# 			files_list.append(result)
 
 
-		return files_list
+	# 	return files_list
 		
-	def sub_dir_path(self, a_dir):
-		return filter(os.path.isdir, [os.path.join(a_dir,f) for f in os.listdir(a_dir)])
+	# def sub_dir_path(self, a_dir):
+	# 	return filter(os.path.isdir, [os.path.join(a_dir,f) for f in os.listdir(a_dir)])
 
-	def get_open_folders(self):
-		# get the opened folders from the side bar
-		folders = self.window.folders()
-		folder_list = []
-		for folder in folders:
-			if folder.lower().endswith('sites'):
-				folder_list += list(self.sub_dir_path(folder))
-			else:
-				folder_list.append(folder)
+	# def get_open_folders(self):
+	# 	# get the opened folders from the side bar
+	# 	folders = self.window.folders()
+	# 	folder_list = []
+	# 	for folder in folders:
+	# 		if folder.lower().endswith('sites'):
+	# 			folder_list += list(self.sub_dir_path(folder))
+	# 		else:
+	# 			folder_list.append(folder)
 
-		return folder_list
+	# 	return folder_list
 
 
 	
--- ModuleAutoComplete.py
+++ ModuleAutoComplete.py
@@ -1,33 +1,139 @@
-import sublime, sublime_plugin, os 
+import sublime, sublime_plugin, os
+import re
+
+class DoobleIO():
+	@staticmethod
+	def is_admin(file_name):
+		lis = file_name.split("\\")
+		if 'admin' in file_name:
+			return True
+		return False
+
+	@staticmethod
+	def is_module(file_name):
+		lis = file_name.split("\\")
+		if 'module' in file_name:
+			return True
+		return False
+
+	@staticmethod
+	def is_ui(sel):
+		return re.match(r'\[\[ui/?.+\]\]', sel, re.IGNORECASE)
+		
+
+
+	@staticmethod
+	def cut_path(file_name):
+		lis = []
+		left_path = ""
+		right_path = ""
+		if 'sites' in file_name:
+			lis = file_name.split('sites')
+			left_path = lis[0]
+			print('sites' + lis[1])
+			right_path = 'sites' + lis[1]
+		elif 'site' in file_name:
+			lis = file_name.split('site')
+			left_path = lis[0]
+			right_path = 'site' + lis[1]
+
+		return(left_path, right_path)
+
+	@staticmethod
+	def check_file_type(file_n):
+		left_path, file_name = DoobleIO.cut_path(file_n)
+		print("left path: " + left_path)
+		print("file name of cut: " + file_name)
+
+		if DoobleIO.is_module(file_name):
+			a_type = '\\Content\\Modules'
+			print("im module file")
+		elif DoobleIO.is_admin(file_name):
+			a_type = '\\Admin'
+			print("im admin file")
+		return a_type
+
+
+
+
 
 class ModuleAutoCompleteCommand(sublime_plugin.EventListener):
 	SCOPE_NAME = 'text.html.pp.module';
-	MODULES_ROOT = '\Content\Modules';
+	
 
 	def on_query_completions(self, view, prefix, locations):
+		# print(locations)
+		# print(view.scope_name(locations[0]))
 		if self.SCOPE_NAME in view.scope_name(locations[0]):
-			autocomplete_list = self.getDirs();
+			autocomplete_list = self.getDirs(view);
 			return autocomplete_list;
 
-	def getDirs(self):
-		modulesList = [];
-		folders = sublime.active_window().folders();
+	def getDirs(self, view):
+		# get user selection
+		selection = self.user_selection(view)
+		# check if it's ui controls
+		is_ui = DoobleIO.is_ui(selection)
+
+		modulesList = []
+		folders = sublime.active_window().folders()
 		# print (folders[0].lower().endswith('sites'))
 		if folders[0].lower().endswith('sites'):
-			folders = get_immediate_subdirectories(self,folders[0])
+			folders = self.get_immediate_subdirectories(folders[0])
+		# print(folders)
+		# check if it's module or admin or ui
+		file_name = sublime.active_window().active_view().file_name().lower()
+		# print("file name: " + file_name)
+		a_type = DoobleIO.check_file_type(file_name)
+		# get all sites folders
 		for x in folders:
 			if x.endswith('dev'):
-				continue;
-			fullPath = x+self.MODULES_ROOT;
-			for dir in os.listdir(fullPath):
-				if '.' not in dir:
-					for fileName in os.listdir(fullPath+'\\'+dir):
-						mPath = dir+'/'+os.path.splitext(fileName)[0];
-						if '.' not in mPath:
-							modulesList.append([mPath,mPath]);
-		return modulesList;
-def get_immediate_subdirectories(self, a_dir):
-    return [a_dir+'\\'+name for name in os.listdir(a_dir)
-            if os.path.isdir(os.path.join(a_dir, name))]
-	
+				continue
+			# print("folder: " + x)
+			# add module path to the sites 
+			# fullPath = x+self.MODULES_ROOT
+			fullPath = x + a_type
+			# print("type: " + a_type)
+			# print("full path: " + fullPath)
+			# in case its ui
+			if is_ui:
+				# print(x + '\\uicontrols')
+				modulesList = self.get_ui_files(x + '\\uicontrols')
+			else:
+				ui_controls_list = self.get_ui_files(x + '\\uicontrols')
+				modulesList = self.get_modules(fullPath) + ui_controls_list
 		
+		return modulesList
+	def get_immediate_subdirectories(self, a_dir):
+    		return [a_dir+'\\'+name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+	def get_modules(self, fullPath):
+		modulesList = []
+		for dir in os.listdir(fullPath):
+			if '.' not in dir:
+				# \Sites\natanzon\Content\Modules\API
+				for fileName in os.listdir(fullPath+'\\'+dir):
+					mPath = dir+'/'+os.path.splitext(fileName)[0]
+					if '.' not in mPath:
+						# print([mPath,mPath])
+						modulesList.append([mPath,mPath])
+		return modulesList
+
+
+	def user_selection(self, view):
+		# get the current cursor point
+		sel = view.sel()[0].begin()
+		# get the line with current cursor
+		get_line = view.line(sel)
+		# convert the line to string
+		sel = view.substr(get_line)
+		# print("user selection: " + sel)
+		return sel
+
+	def get_ui_files(self, folder):
+		modulesList = []
+		for fileName in os.listdir(folder):
+			mPath = 'ui/' + os.path.splitext(fileName)[0]
+			modulesList.append([mPath,mPath])
+		return modulesList
+
